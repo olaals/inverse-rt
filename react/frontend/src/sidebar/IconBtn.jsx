dda2e54591905d38dfaf61a6dbf569a4a5673c7b
Ola Alstad <olaals@dhcp-10-24-13-97.wlan.ntnu.no>
2022-01-08 14:50:53 +0100

    Add progress

---
+++ react/frontend/src/sidebar/IconBtn.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Md3DRotation } from "react-icons/md";
+
+const IconBtn = () => {
+    return (
+        <div className="iconBtn">
+            <Md3DRotation className="icon" />
+        </div>
+    )
+}
+
+export default IconBtn
+
--- react/frontend/src/sidebar/SidebarSelect.jsx
+++ react/frontend/src/sidebar/SidebarSelect.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import IconBtn from './IconBtn'
 
 const SidebarSelect = () => {
     return (
         <div className="sidebarSelect">
-
+            <IconBtn />
         </div>
     )
 }
