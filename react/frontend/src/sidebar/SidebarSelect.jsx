818d773ea91bc85fb8c50b49f3d2ef802469289d
Ola Alstad <olaalstad96@gmail.com>
2022-01-14 13:26:19 +0100

    Add icons to sidebar select

--- react/frontend/src/sidebar/SidebarSelect.jsx
+++ react/frontend/src/sidebar/SidebarSelect.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { IoIosApps } from "react-icons/io";
+import { FaWrench } from "react-icons/fa";
+import { GiMagnifyingGlass } from "react-icons/gi";
+import "./css/icons.css"
 
 const SidebarSelect = () => {
+    const logClick = () => {
+        console.log("Clicked!");
+    }
     return (
+
         <div className="sidebarSelect">
+            <div className="icon-div" onClick={logClick}>
+                <IoIosApps className="icon-sidebar-select" />
+            </div>
+            <div className="icon-div">
+                <FaWrench className="icon-sidebar-select" />
+            </div>
+            <div className="icon-div">
+                <GiMagnifyingGlass className="icon-sidebar-select" />
+            </div>
+
+
 
         </div>
     )
