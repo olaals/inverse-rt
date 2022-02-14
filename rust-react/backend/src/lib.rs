0fc2d91607ee9fd1befc3ffc983295b5e15c6169
Ola Alstad <olaals@MacBook-Air.localdomain>
2022-02-14 21:00:58 +0100

    Add progress

--- rust-react/backend/src/lib.rs
+++ rust-react/backend/src/lib.rs
@@ -1,1 +1,2 @@
-
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+pub mod main_api;
---
+++ rust-react/backend/src/main_api.rs
@@ -0,0 +1,12 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    println!("{:?}", req_body);
+    HttpResponse::Ok().body(req_body)
+}
